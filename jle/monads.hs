84cd06810d717ee3d7cc787fd0be5e9df86ea795
Gene Le <gene.gkl@gmail.com>
2014-06-18 20:35:19 -0500

    playing with Maybe's

--- jle/monads.hs
+++ jle/monads.hs
@@ -1,8 +1,21 @@
+divideMaybe :: Int -> Int -> Maybe Int
+divideMaybe _ 0 = Nothing
+divideMaybe x y = Just (x `div` y)
+
+headMaybe :: [a] -> Maybe a
+headMaybe [] = Nothing
+headMaybe (x:_) = Just x
+
+halveMaybe :: Int -> Maybe Int
+halveMaybe a
+  | a `mod` 2 == 0 = Just (a `div` 2)
+  | otherwise = Nothing
+
 double :: Int -> Int
 double x = x * 2
 
 inMaybe :: (a -> b) -> (Maybe a -> Maybe b)
-inMaybe f maybeVar = liftedF maybeVar
+inMaybe f = liftedF
   where
     liftedF (Just x) = Just (f x)
     liftedF Nothing  = Nothing
--- learn-you-a-haskell/ch-3-SyntaxinFunctions/as_patterns.hs
+++ learn-you-a-haskell/ch-3-SyntaxinFunctions/as_patterns.hs
@@ -1,3 +1,3 @@
 firstLetter :: String -> String
 firstLetter "" = "Empty string, let's get a drink and break dance!"
-firstLetter wholeString@(x:xs) = "The first letter of " ++ wholeString ++ " is " ++ [x]
+firstLetter wholeString@(firstChar:theRest) = "The first letter of " ++ wholeString ++ " is " ++ [firstChar]
