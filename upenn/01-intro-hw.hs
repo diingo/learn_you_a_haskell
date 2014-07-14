c8f2852f51793fd237cfc865289a72139cc0c272
Gene Le <gene.gkl@gmail.com>
2014-07-13 22:40:49 -0500

    toDigitsRev function - complete

--- upenn/01-intro-hw.hs
+++ upenn/01-intro-hw.hs
@@ -10,10 +10,18 @@ toDigits n
 readInt :: String -> Integer
 readInt n = read n :: Integer
 
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev n
+  | n <= 0 = []
+  | otherwise = rev (toDigits n)
+
+rev :: [Integer] -> [Integer]
+rev [] = []
+rev (x:xs) = (rev xs) ++ [x]
+
+
 
 --readInt n = read (map (:"") n)
-
---toDigitsRev :: Integer -> [Integer]
 
 --dot :: (b -> c) -> (a -> b) -> a -> c
 --dot f g x = f (g x)
