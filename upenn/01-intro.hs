afd463c9295208d42b5c86b78bfd9cfa14398b76
Gene Le <gene.gkl@gmail.com>
2014-07-13 21:12:56 -0500

    playing with code from 01-intro

--- upenn/01-intro.hs
+++ upenn/01-intro.hs
@@ -28,3 +28,19 @@ hailstone' :: Integer -> Integer
 hailstone' n
   | isEven' n = n `div` 2
   | otherwise      = 3*n + 1
+
+hailstoneSeq :: Integer -> [Integer]
+hailstoneSeq 1 = [1]
+hailstoneSeq n = n : hailstoneSeq (hailstone' n)
+
+intListLength :: [Integer] -> Integer
+intListLength [] = 0
+intListLength (x:xs) = 1 + intListLength xs
+
+hailstoneLen :: Integer -> Integer
+hailstoneLen n = intListLength (hailstoneSeq n) - 1
+
+sumEveryTwo :: [Integer] -> [Integer]
+sumEveryTwo [] = []
+sumEveryTwo (x:[]) = [x]
+sumEveryTwo (x:(y:zs)) = (x+y) : sumEveryTwo zs
